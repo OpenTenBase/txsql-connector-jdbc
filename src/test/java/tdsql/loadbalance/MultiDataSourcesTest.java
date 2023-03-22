20ef716ecda73996a9d07c2103027deb3a3c171f
TDavid2509 <58974144+TDavid2509@users.noreply.github.com>
2023-03-22 18:52:57 +0800

    1. 写了一个测试用例，测试多数据源bug

---
+++ src/test/java/tdsql/loadbalance/MultiDataSourcesTest.java
@@ -0,0 +1,21 @@
+package tdsql.loadbalance;
+
+import java.sql.Connection;
+
+public class MultiDataSourcesTest {
+
+    private String proxy1 = "9.30.0.250:15003";
+
+    private String proxy2 = "9.30.2.116:15003";
+
+    private String proxy3 = "9.30.2.89:15003";
+
+    private String proxy4 = "9.30.2.94:15003";
+
+    private Connection getConnection(String url) {
+        return null;
+    }
+    private void execute(String url) {
+
+    }
+}
